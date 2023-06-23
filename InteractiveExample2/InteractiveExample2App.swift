daa4b4a7dec898ae93ab00f4ce1c13d5a42fe507
Carolyn Hernandez <carolynhernadez019@gmail.com>
2023-06-23 10:34:38 -0700

    Initial Commit

---
+++ InteractiveExample2/InteractiveExample2App.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveExample2App.swift
+//  InteractiveExample2
+//
+//  Created by Carolyn Hernandez on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveExample2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
