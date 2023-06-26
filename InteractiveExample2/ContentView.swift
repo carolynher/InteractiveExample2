14261be60db6e7d3b6a4841050f334282b695b90
Carolyn Hernandez <carolynhernadez019@gmail.com>
2023-06-26 07:59:50 -0700

    Changed button color

--- InteractiveExample2/ContentView.swift
+++ InteractiveExample2/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //this variable is called a property because it is part of a Struct ; State is property wrapper
+    //property wrapper = wraps a property and gives it additional behvior
+    // @State property --> used to represent the internal state of a SwiftUI View, & to automatically make a view update when that state is changed - whenever the property changes, the UI must update
+    //it's recommended to be a private var --> emphasizes that the state does not leave the view it was created in (instead of being local)
+    @State private var name = ""
+    @State private var titleText = "What is your name?"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        //adding spacing to all views/controls
+        VStack(spacing: 100) {
+            //refers to property variable
+            Text(titleText)
+                .font(.title)
+            
+            //a Conrol --> allows for user interactions
+            //uses two-way binding --> binds TextField so that it shows the value of the name property & binds name property so any changes to the TextField update the name property
+            //two-way binding requires '$'
+            TextField("Type name here...", text: $name) // the text is asking where we would like to store the user's input text (ex. in a specific variable)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .border(Color.gray, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+            
+            //a Control; a button initiates an action (action = indicate what we would like accomplished when the user taps the button); in this case we want it to update the title to display the user's name
+            Button("Submit Name") {
+                titleText = "Welcome, \(name)!"
+            }
+            //adding modifiers to button
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
+
         }
+        //add padding so text field does not touch the edge
         .padding()
+     
     }
 }
 
@@ -24,3 +52,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
